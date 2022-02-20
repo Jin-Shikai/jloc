72c665c10cb7cc8e5acc8142c59ecda7fceb4023
jinshikai <1784257883@qq.com>
2022-02-20 22:04:25 +0800

    parse & getter

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 mod test;
 
+use serde_json::Value;
 use std::error::Error;
 use std::io;
+/*
+   file: src/lib.rs
+   author: jinshikai
+*/
 
 /*
-   author: jinshikai
    create date: 2/16/22
    desc: 运行函数主体
 */
@@ -18,4 +22,72 @@ pub fn run() -> Result<(), Box<dyn Error>> {
         println!("{}", input.trim());
         input.clear();
     }
+}
+
+/*
+   create date: 2/20/22
+   desc: Value::Object的key的数据结构，可能为string或usize
+*/
+#[derive(Debug, PartialEq)]
+pub enum JsonKey {
+    Str(String),
+    Idx(usize),
+}
+
+/*
+   create date: 2/19/22
+   desc: 从一个json中按vec的元素作为key依次取出value，中途遇到任何问题返回空；
+   notice: 不区分缺失key/key存在但value为null这两种情况，一律返回Value::Null
+*/
+pub fn getter_from_vec<'a>(v: &'a Value, key_list: &Vec<JsonKey>) -> &'a Value {
+    let mut ans = v;
+    for key in key_list {
+        match key {
+            JsonKey::Str(key) => {
+                let next = ans.get(key);
+                match next {
+                    None => return &Value::Null,
+                    _ => {
+                        ans = next.unwrap();
+                        continue;
+                    }
+                }
+            }
+            JsonKey::Idx(key) => {
+                let next = ans.get(key);
+                match next {
+                    None => return &Value::Null,
+                    _ => {
+                        ans = next.unwrap();
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+/*
+   create date: 2/20/22
+   desc: 解析一个字符串，返回一个vec:<JsonKey>
+        1. 以'.'分隔每个item为一个vec
+        2. 如果item以'['开头且以']'结尾且中间部分能转为数字，作为JsonKey::Idx;
+        3. 其他情况作为JsonKey::Str;
+*/
+pub fn parse_key(raw_arg: String) -> Vec<JsonKey> {
+    let v_raw: Vec<&str> = raw_arg.split(".").collect();
+    let mut v_key: Vec<JsonKey> = Vec::new();
+    for item in v_raw {
+        let n = item.len();
+        if n > 2 && &item[0..1] == "[" && &item[n - 1..n] == "]" {
+            match &item[1..n - 1].parse() {
+                Ok(num) => v_key.push(JsonKey::Idx(*num)),
+                Err(_) => v_key.push(JsonKey::Str(item[1..n - 1].to_string())),
+            };
+        } else {
+            v_key.push(JsonKey::Str(item.to_string()));
+        }
+    }
+    return v_key;
 }
--- src/test.rs
+++ src/test.rs
@@ -3,38 +3,154 @@
     create date: 2/17/22
     desc: 测试驱动开发
 */
+
 #[cfg(test)]
 mod test {
-    use serde_json::{json, Value};
-    use std::io;
+    use serde_json::*;
+    use std::*;
 
-    // if str can be parsed as json, get json
+    use crate::*;
+
     #[test]
-    fn parse_json() {
-        let contents = r#"{
-            "name": "Shikai Jin",
-            "age": "23",
-            "age_int":23
-        }"#;
-        let v: Value = serde_json::from_str(contents).unwrap();
-        assert_eq!("Shikai Jin", v["name"]);
-        assert_eq!("23", v["age"]);
-        assert_eq!(23, v["age_int"]);
+    fn test_parse_json() {
+        let contents: Value = json!(
+            {
+                "name": "Shikai Jin",
+                "age": "23",
+                "age_int":23
+            }
+        );
+        assert_eq!("Shikai Jin", contents["name"]);
+        assert_eq!("23", contents["age"]);
+        assert_eq!(23, contents["age_int"]);
     }
 
-    // elif can't be parsed as json, get Null
     #[test]
-    fn parse_general_str() {
+    fn test_parse_general_str() {
         let contents = "abcd";
         let v: Value = serde_json::from_str(contents).unwrap_or(Value::Null);
         assert_eq!(json!(Value::Null), v);
     }
 
     #[test]
-    fn parse_general_int() {
-        assert_eq!(json!("1234"), "1234");
+    fn test_parse_general_int() {
         let contents = "1234";
         let v: Value = serde_json::from_str(contents).unwrap_or(Value::Null);
         assert_eq!(json!(1234), v);
+        assert_eq!(json!("1234"), "1234");
+    }
+
+    #[test]
+    fn test_format_output() {
+        let input = json!(
+            {
+                "a boolean": false,
+                "an array": [3, 2, 1],
+                "child_json":{"test_key" :"test_value"}
+            }
+        );
+        let mut serializer = serde_json::Serializer::pretty(io::stdout());
+        serde_transcode::transcode(input, &mut serializer).unwrap();
+    }
+
+    #[test]
+    fn test_getter() {
+        let input = json!(
+            {
+                "a boolean": false,
+                "an array": [3, 2, 1],
+                "child_json":{"test_key" :"test_value"}
+            }
+        );
+        assert_eq!(json!(false), *input.get("a boolean").unwrap());
+        assert_eq!(json!([3, 2, 1]), *input.get("an array").unwrap());
+        assert_eq!(json!(3), *input.get("an array").unwrap().get(0).unwrap());
+        assert_eq!(None, input.get(0));
+        assert_eq!(None, input.get("not exisit"));
+    }
+
+    #[test]
+    fn test_getter_from_vec() {
+        let input = json!(
+            {
+                "a boolean": null,
+                "an array": [3, 2, 1],
+                "child_json":{"test_key" :"test_value"}
+            }
+        );
+        let key_list_corr_1: Vec<JsonKey> =
+            vec![JsonKey::Str("an array".to_string()), JsonKey::Idx(0)];
+        assert_eq!(json!(3), *getter_from_vec(&input, &key_list_corr_1));
+
+        let key_list_corr_2: Vec<JsonKey> = vec![JsonKey::Str("a boolean".to_string())];
+        assert_eq!(Value::Null, *getter_from_vec(&input, &key_list_corr_2));
+
+        let key_list_corr_3: Vec<JsonKey> = vec![
+            JsonKey::Str("child_json".to_string()),
+            JsonKey::Str("test_key".to_string()),
+        ];
+        assert_eq!(
+            json!("test_value"),
+            *getter_from_vec(&input, &key_list_corr_3)
+        );
+        let key_list_err: Vec<JsonKey> =
+            vec![JsonKey::Str("not exisit".to_string()), JsonKey::Idx(0)];
+        assert_eq!(Value::Null, *getter_from_vec(&input, &key_list_err));
+    }
+
+    #[test]
+    fn test_parse_key() {
+        let arg_str = "a.b.[1].c.d";
+        let target_vec: Vec<JsonKey> = vec![
+            JsonKey::Str("a".to_string()),
+            JsonKey::Str("b".to_string()),
+            JsonKey::Idx(1),
+            JsonKey::Str("c".to_string()),
+            JsonKey::Str("d".to_string()),
+        ];
+        assert_eq!(target_vec, parse_key(arg_str.to_string()));
+    }
+
+    #[test]
+    fn test_parse_and_get() {
+        let input = json!(
+            {
+                "key1": "a",
+                "key2": [3, 2, 1],
+                "key3": {
+                    "test_key": "test_value"
+                },
+                "key4": [{
+                    "k4_1": "v4_1"
+                }, {
+                    "k4_2": "v4_2"
+                }],
+                "key5": [
+                    "a",
+                    "b",
+                    "c"
+                ]
+            }
+        );
+        let mut arg_str = "key3.test_key";
+        assert_eq!(
+            json!("test_value"),
+            *getter_from_vec(&input, &parse_key(arg_str.to_string()))
+        );
+        arg_str = "key1";
+        assert_eq!(
+            json!("a"),
+            *getter_from_vec(&input, &parse_key(arg_str.to_string()))
+        );
+        arg_str = "key2.[0]";
+        assert_eq!(
+            json!(3),
+            *getter_from_vec(&input, &parse_key(arg_str.to_string()))
+        );
+        arg_str = "key4.[0].k4_1";
+        assert_eq!(
+            json!("v4_1"),
+            *getter_from_vec(&input, &parse_key(arg_str.to_string()))
+        );
     }
 }
